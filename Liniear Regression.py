b54aed21e8d8fa250de1fa8258adb2b1184f2398
Rheinlethal <farhanduta060402@gmail.com>
2025-11-20 12:22:52 +0700

    update

---
+++ Liniear Regression.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# ============================
+# 1. BACA CSV
+# ============================
+
+df = pd.read_csv("data tembak1_Sheet1.csv")
+
+# ============================
+# 2. AMBIL KOLOM FITUR & TARGET
+# ============================
+
+x = df[['distance','enemy_speed','angle','shell_velocity','maximum_dispersion']].to_numpy()
+z = df['impact_offset_x'].to_numpy()
+
+# ============================
+# 3. TRAIN MODEL
+# ============================
+
+model = LinearRegression()
+model.fit(x,z)
+
+# ============================
+# 4. TAMPILIN RUMUS
+# ============================
+
+print("=== HASIL RUMUS ===")
+print("Coefficient (coef):", model.coef_)
+print("Intercept:", model.intercept_)
